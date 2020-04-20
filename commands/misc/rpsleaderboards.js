b9cebe7d191dc22639925d0baaf4e4f46ae11108
58866873 <jamesthackett1@gmail.com>
2020-04-20 19:40:10 +0100

    added misc commands

---
+++ commands/misc/rpsleaderboards.js
@@ -0,0 +1,32 @@
+const storage = require("storage-to-json");
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+  config: {
+    name: "rpsleaderboards",
+    description: "Find out your ranking!",
+    category: "miscellaneous",
+    aliases: ["rpsl"],
+  },
+  run: async (bot, message, args) => {
+    const leaderboards = new storage(`rpsLeaderboards`);
+
+    const embed = new RichEmbed()
+      .setTitle("Leaderboards of Rock, Paper, Scissors!")
+      .setColor("GREEN")
+      .setFooter(
+        `© ${message.guild.me.displayName} | Developed By JammyGamer`,
+        bot.user.displayAvatarURL
+      );
+
+    leaderboards.each(function (value, key) {
+      embed.addField(
+        `${bot.users.fetch(key).username}`,
+        `Won | Drawn | Lost 	\`${value}\``,
+        true
+      );
+    });
+
+    message.channel.send(embed);
+  },
+};
