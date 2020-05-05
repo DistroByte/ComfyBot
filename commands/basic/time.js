f8b37b8f033c045366bcf3b64af4cc70614b4012
James-Hackett <jamesthackett1@gmail.com>
2020-05-05 22:10:03 +0100

    :sparkles: Feat: adds a time command with timezones

---
+++ commands/basic/time.js
@@ -0,0 +1,30 @@
+var moment = require('moment-timezone');
+const Storage = require('storage-to-json');
+const store = new Storage('userTime');
+
+module.exports = {
+	config: {
+		name: 'time',
+		usage: '(setzone) (zone)',
+		description:
+			'Gets local time and UTC time. Also allows you to set a local timezone. Go to https://momentjs.com/timezone/ to find your timezone',
+		category: 'basic',
+	},
+	run: async (bot, message, args) => {
+		let id = message.author.id;
+
+		if (args[0] === 'setzone') {
+			let userTime = args[1].toString();
+			store.set(id, userTime);
+			message.channel
+				.send(`${args[1]} is now saved as your local timezone`)
+				.then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
+			return;
+		}
+		let timezone = store.get(id);
+		let mL = moment().tz(timezone);
+		let mU = moment.utc();
+
+		message.channel.send(`Local: ${mL}\nUTC: ${mU}`);
+	},
+};
