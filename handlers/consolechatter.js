3d0c966abcf61fa905d8ad77e92711a7181eb6be
James-Hackett <jamesthackett1@gmail.com>
2020-05-17 21:49:40 +0100

    :fire: removes console chatter code

--- handlers/consolechatter.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-	let prompt = process.openStdin();
-	prompt.addListener('data', (res) => {
-		let x = res.toString().trim().split(/ +/g);
-		bot.channels.cache.get('687688180597653639').send(x.join(' '));
-	});
-};
