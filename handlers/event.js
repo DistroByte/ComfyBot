0e4c9af0572eecc9220543dff3c58b3dc6c7aebb
James-Hackett <jamesthackett1@gmail.com>
2020-05-17 17:46:46 +0100

    :sparkles: Feat: adds bot loading to files

--- handlers/event.js
+++ handlers/event.js
@@ -11,5 +11,5 @@ module.exports = (bot) => {
       bot.on(eName, evt.bind(null, bot));
     }
   };
-  ["client", "guild"].forEach((x) => load(x));
+  ["client", "message", "members", "reactions"].forEach((x) => load(x));
 };
