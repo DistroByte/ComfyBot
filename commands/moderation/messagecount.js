066e1760bfe6bc5b4a82fe5a67ec31d3ee3db661
James-Hackett <jamesthackett1@gmail.com>
2020-09-28 16:27:57 +0100

    :construction: WIP

--- commands/moderation/messagecount.js
+++ commands/moderation/messagecount.js
@@ -11,26 +11,28 @@ module.exports = {
 	},
 	run: async (bot, message, args) => {
 		if (!message.member.hasPermission(['ADMINISTRATOR']))
-			return message.channel.send(
-				'You dont have permission to perform this command!'
-			);
+			return message.channel.send('You dont have permission to perform this command!');
 
 		const count = new storage(`${message.guild.id}`);
 		let embed = new MessageEmbed()
 			.setTitle(`Message Count for ${message.guild.name}`)
 			.setColor('GREEN')
 			.setThumbnail(message.guild.iconURL)
-			.setFooter(
-				`© ${message.guild.me.displayName} | Developed By JammyGamer`,
-				bot.user.displayAvatarURL()
-			)
+			.setFooter(`© ${message.guild.me.displayName} | Developed By DistroByte`, bot.user.displayAvatarURL())
 			.setTimestamp();
 
 		let regexTotal = /Total/;
 		let date = new Date().toString().slice(4, 24);
 		let runningTotal = 0;
-		let oldTotal = count.get('Total').slice(20);
-		let lastDate;
+
+		let objArray = [];
+		count.each((v, k) => {
+			let obj = { v, k }
+			objArray.push(obj);
+			console.log(obj);
+		});
+		console.log(objArray);
+
 		count.each(function (value, key) {
 			if (!regexTotal.test(key)) {
 				let userKey;
@@ -41,15 +43,11 @@ module.exports = {
 				}
 				embed.addField(`${userKey}`, `Messages sent: \`${value}\``, true);
 				runningTotal += value;
-			}
-			if (regexTotal.test(key)) {
+			} else if (regexTotal.test(key)) {
 				lastDate = value.slice(0, 20);
 			}
 		});
-		embed.setDescription(
-			`Current time and date: ${date}\nTotal messages since ${lastDate}: \`${runningTotal - oldTotal
-			}\`\nTotal messages on server: \`${runningTotal}\``
-		);
+		embed.setDescription(`Total messages on server: \`${runningTotal}\``);
 		count.set(`Total`, `${date} ${runningTotal}`);
 		message.channel.send(embed);
 	},
