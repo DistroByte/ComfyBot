c2cb9d747523b35a87623e2e909da3413c11fb69
James-Hackett <jamesthackett1@gmail.com>
2020-09-29 22:58:49 +0100

    :fire: Removes useless code

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,15 +1,6 @@
-const Store = require('storage-to-json');
-const guildData = new Store('guildData')
-
-module.exports = async (client, member) => {
+module.exports = async (bot, member) => {
   let guildId = member.guild.id
-  let fetchedGuildId = guildData.get(guildId)
-  if (fetchedGuildId) {
-    if (fetchedGuildId.hasOwnProperty(defaultChannel)) {
-      let channel = await member.guild.channels.get(fetchedGuildId.defaultChannel)
-      channel.send(`Welcome to the server ${member.username}`)
-    }
-  } else {
-
+  if (guildId === "759921793422458901") {
+    member.send('Hi there! Welcome to the 2024 Computer Applications Discord server! Before we continue, I need to verify you are who you say you are :eyes:. Please reply with your student number (CAO number)');
   }
 }
