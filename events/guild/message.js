6e43321088a654247fb000ad9bc9e9adff498aaf
James-Hackett <jamesthackett1@gmail.com>
2020-09-29 22:48:22 +0100

    :sparkles: Feat: add verification to a server

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,6 +1,7 @@
 const { prefix } = require('../../botconfig.json');
 const storage = require('storage-to-json');
 const correct = new storage('correct');
+const podNumbers = new storage('podNumbers')
 
 module.exports = async (bot, message) => {
 	let args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -18,23 +19,43 @@ module.exports = async (bot, message) => {
 		}
 	}
 
-	const count = new storage(`${message.guild.id}`);
-	let date = new Date().toString().slice(4, 24);
-	if (!count.get('Total')) {
-		count.set(`Total`, `${date} 1`);
-	}
-	if (!count.get(`${message.author.id}`)) {
-		let messageCount = 1;
-		count.set(`${message.author.id}`, messageCount);
-	} else {
-		let messageCount = count.get(`${message.author.id}`);
-		messageCount++;
-		count.set(`${message.author.id}`, messageCount);
-	}
+	if (message.guild !== null) {
 
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-	let commandfile =
-		bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
-	if (commandfile) commandfile.run(bot, message, args);
+		const count = new storage(`${message.guild.id}`);
+		let date = new Date().toString().slice(4, 24);
+		if (!count.get('Total')) {
+			count.set(`Total`, `${date} 1`);
+		}
+		if (!count.get(`${message.author.id}`)) {
+			let messageCount = 1;
+			count.set(`${message.author.id}`, messageCount);
+		} else {
+			let messageCount = count.get(`${message.author.id}`);
+			messageCount++;
+			count.set(`${message.author.id}`, messageCount);
+		}
+
+		if (message.author.bot) return;
+		if (!message.content.startsWith(prefix)) return;
+		let commandfile =
+			bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
+		if (commandfile) commandfile.run(bot, message, args);
+	} else {
+		if (message.author.bot) return
+		let number = [];
+		podNumbers.each((v, k) => {
+			number.push(k);
+		});
+		let toFind = number.find(v => v === message.content)
+		if (!toFind) {
+			message.reply('Sorry! It seems like you are not in this course! Thanks for stopping by :slight_smile:');
+		} else {
+			let CAGuild = bot.guilds.cache.find(guild => guild.id === "759921793422458901");
+			let userToVerify = CAGuild.members.cache.find(u => u.id === message.author.id)
+			userToVerify.roles.add("760604574217273415");
+			message.reply('Success! Welcome to the server!');
+			console.log('New member added!');
+			podNumbers.remove(toFind);
+		}
+	}
 };
