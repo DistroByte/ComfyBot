afb3a768cb13de227e53081d10336788139dcc31
James-Hackett <jamesthackett1@gmail.com>
2020-10-06 13:36:59 +0100

    :speech_baloon: Updates readability

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,7 @@
 module.exports = async (bot, member) => {
   let guildId = member.guild.id
   if (guildId === "759921793422458901") {
-    member.send('Hi there! Welcome to the 2024 Computer Applications Discord server! Before we continue, I need to verify you are who you say you are :eyes:. Please reply with your student number (CAO number)');
+    member.send('Hi there! Welcome to the 2024 Computer Applications Discord server! Before we continue, I need to verify you are who you say you are :eyes:\nPlease reply with !verify <student number (CAO number)> (ignore the angle brackets)!');
+    member.send('https://imgur.com/a/KkD35jb');
   }
 }
