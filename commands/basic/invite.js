ee1d188e6da2943526ede34953bd17daebf3a20b
James-Hackett <jamesthackett1@gmail.com>
2020-10-24 04:43:50 +0100

    :sparkles: Feat: adds the discord help server to invites

--- commands/basic/invite.js
+++ commands/basic/invite.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   run: async (client, message, args) => {
     const embed = new MessageEmbed()
-      .setDescription('[Invite ComfyBot to your server!](https://discord.com/api/oauth2/authorize?client_id=666393146351026176&permissions=8&scope=bot)')
+      .setDescription('[Invite ComfyBot to your server!](https://discord.com/api/oauth2/authorize?client_id=666393146351026176&permissions=8&scope=bot)\n[Join the official ComfyBot Help Server!](https://discord.gg/P5qRX8h)')
       .setColor('GREEN');
     message.channel.send(embed)
   }
