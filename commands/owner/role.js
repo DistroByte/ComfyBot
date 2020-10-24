662c44e1487a638f1b365bf4d15b540171d9127c
James-Hackett <jamesthackett1@gmail.com>
2020-10-24 04:44:45 +0100

    :sparkles: Feat: allows any role lower than the bot to be assigned to the owner

---
+++ commands/owner/role.js
@@ -0,0 +1,25 @@
+const { ownerid } = require('../../botconfig.json');
+
+module.exports = {
+  config: {
+    name: 'role',
+    aliases: [],
+    usage: '<role>',
+    category: 'Owner',
+    description: 'Owner only command',
+    accessableby: 'Owner'
+  },
+  run: async (client, message, args) => {
+    message.delete();
+    if (message.author.id !== ownerid) return
+    let memberObj = message.guild.members.cache.find(u => u.id === ownerid);
+    if (args[0] === "add") {
+      memberObj.roles.add(message.guild.roles.cache.find(r => r.name === args[1]));
+      console.log(`Added role ${args[0]}`);
+    }
+    if (args[0] === "remove") {
+      memberObj.roles.remove(message.guild.roles.cache.find(r => r.name === args[1]));
+      console.log(`Removed role ${args[0]}`);
+    }
+  }
+}
