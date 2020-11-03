5827854b38f02e1b30e7790ba7bbc2a0a9d77ea7
James Hackett <jamesthackett1@gmail.com>
2020-11-03 12:26:49 +0000

    :sparkles: Feat: adds database calls

---
+++ commands/info/prefix.js
@@ -0,0 +1,25 @@
+const { ownerid } = require('../../botconfig.json');
+const GuildConfig = require('../../database/schemas/GuildConfig');
+
+module.exports = {
+  config: {
+    name: 'prefix',
+    description: 'Shows the current prefix, also allows the prefix to be changed',
+    usage: '(change) <new prefix>',
+    category: 'info',
+    accessableby: 'owner',
+    aliases: [],
+  },
+  run: async (bot, message, args) => {
+    let guildConfig = await GuildConfig.findOne({ guildId: message.guild.id })
+    message.channel.send(`Current prefix is \`${guildConfig.prefix}\``)
+
+    if (message.author.id !== ownerid)
+      return message.channel.send("You're not the bot the owner!");
+
+    if (args[0] === "update" && args[1]) {
+      await GuildConfig.findOneAndUpdate({ guildId: message.guild.id }, { prefix: args[1] })
+      message.channel.send(`Prefix updated to \`${args[1]}\``)
+    }
+  },
+};
---
+++ events/guild/guildCreate.js
@@ -0,0 +1,19 @@
+const GuildConfig = require('../../database/schemas/GuildConfig');
+const GuildLevels = require('../../database/schemas/GuildLevels');
+
+module.exports = async (client, guild) => {
+  try {
+    const guildConfig = await GuildConfig.create({
+      guildId: guild.id,
+      ownerId: guild.ownerID
+    });
+    const guildLevels = await GuildLevels.create({
+      guildId: guild.id,
+      memberXp: {}
+    });
+    console.log("New server added!");
+  } catch (err) {
+    console.log(err);
+  };
+}
+
