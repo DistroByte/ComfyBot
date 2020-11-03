864db6d6e26d6af86760c91c319340269a822e00
James Hackett <jamesthackett1@gmail.com>
2020-11-03 12:14:27 +0000

    :sparkles: Feat: adds ask command

---
+++ commands/info/ask.js
@@ -0,0 +1,14 @@
+module.exports = {
+  config: {
+    name: 'ask',
+    aliases: [],
+    usage: '',
+    category: 'basic',
+    description: 'Responds with an image',
+    accessableby: 'Members'
+  },
+  run: async (client, message, args) => {
+    message.channel.send('Don\'t ask to ask, just ask!')
+    message.channel.send('https://cdn.discordapp.com/attachments/762797129626288129/770619652127719464/ask.png')
+  }
+}
