324c2a4b1883ceebb0c2b919253ab13bde3a42c0
James-Hackett <jamesthackett1@gmail.com>
2021-01-25 18:31:42 +0000

    :art: fixes style

--- commands/fun/text.js
+++ commands/fun/text.js
@@ -8,23 +8,20 @@ module.exports = {
     aliases: ['t'],
   },
   run: async (bot, message, args) => {
-	figlet(args.join(' '), function(err, data) {
-		if (args.length === 0) {
-			message.channel.send('Provide text to style with ```!text <text>```');
-			console.log('Error: No args given.')
-			return;
-		}
-		else if (err) {
-			message.channel.send('Something went wrong...');
-			console.dir(err);
-			return;
-		}
-		else if (data.length > 2000) {
-			message.channel.send('Message is too long...');
-			console.log('Error: Message is too long.')
-			return;
-		}
-		message.channel.send('```\r' + data + '\r```');
-	});
+    figlet(args.join(' '), function (err, data) {
+      if (args.length === 0) {
+        message.channel.send('Provide text to style with ```!text <text>```');
+        return;
+      }
+      else if (err) {
+        message.channel.send('Something went wrong...');
+        return;
+      }
+      else if (data.length > 2000) {
+        message.channel.send('Message is too long...');
+        return;
+      }
+      message.channel.send('```\r' + data + '\r```');
+    });
   }
 };
