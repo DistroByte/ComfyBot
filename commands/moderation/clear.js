2d58fdefcd2bc91d4eec21775923cfb1f8a93800
James Hackett <jamesthackett1@gmail.com>
2021-02-05 02:53:18 +0000

    :sparkles: Feat: fixes clear command not clearing specified number of messages

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,49 +1,47 @@
 module.exports = {
-	config: {
-		name: 'clear',
-		description: 'Clears x number of messages',
-		usage: '<number>',
-		category: 'moderation',
-		accessableby: 'Moderators',
-	},
-	run: async (bot, message, args) => {
-		if (message.deletable) {
-			message.delete();
-		}
+  config: {
+    name: 'clear',
+    description: 'Clears x number of messages',
+    usage: '<number>',
+    category: 'moderation',
+    accessableby: 'Moderators',
+  },
+  run: async (bot, message, args) => {
+    message.delete();
 
-		if (!message.member.hasPermission('MANAGE_MESSAGES')) {
-			return message
-				.reply("You can't delete messages....")
-				.then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
-		}
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+      return message
+        .reply("You can't delete messages....")
+        .then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
+    }
 
-		if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-			return message
-				.reply(
-					"Yeah.... That's not a number? I also can't delete 0 messages by the way."
-				)
-				.then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
-		}
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+      return message
+        .reply(
+          "Yeah.... That's not a number? I also can't delete 0 messages by the way."
+        )
+        .then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
+    }
 
-		if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) {
-			return message
-				.reply("Sorry... I can't delete messages.")
-				.then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
-		}
+    if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) {
+      return message
+        .reply("Sorry... I can't delete messages.")
+        .then((m) => m.delete({ timeout: 5000, reason: 'tidying up' }));
+    }
 
-		let deleteAmount;
+    let deleteAmount;
 
-		if (parseInt(args[0]) > 100) {
-			deleteAmount = 100;
-		} else {
-			deleteAmount = parseInt(args[0]);
-		}
+    if (parseInt(args[0]) > 100) {
+      deleteAmount = 100;
+    } else {
+      deleteAmount = parseInt(args[0]);
+    }
 
-		message.channel
-			.bulkDelete(deleteAmount, true)
-			.then((deleted) =>
-				message.channel.send(`I deleted \`${deleted.size}\` messages.`)
-			)
-			.catch((err) => message.reply(`Something went wrong... ${err}`));
-	},
+    message.channel
+      .bulkDelete(deleteAmount, true)
+      .then((deleted) =>
+        message.channel.send(`I deleted \`${deleted.size}\` messages.`)
+      )
+      .catch((err) => message.reply(`Something went wrong... ${err}`));
+  },
 };
