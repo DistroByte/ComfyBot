cb3ed4aee38de2ecd1502ec6258dd74beb289aa5
James Hackett <jamesthackett1@gmail.com>
2021-02-22 20:55:09 +0000

    :sparkles: Feat: adds space

---
+++ commands/fun/space.js
@@ -0,0 +1,89 @@
+const { MessageEmbed } = require('discord.js')
+const fetch = require('node-fetch');
+
+module.exports = {
+  config: {
+    name: 'space',
+    aliases: ['space', 'launch'],
+    usage: '(number (default=3))',
+    category: 'fun',
+    description: 'Fetches the next 3 space launches and returns them in a nice embed',
+    accessableby: 'Members'
+  },
+  run: async (client, message, args) => {
+    const embed = new MessageEmbed()
+      .setTitle("**Upcoming Launches, in order**")
+
+    var url = "https://spacelaunchnow.me/api/3.3.0/launch/upcoming/?format=json";
+    if (args[1] === "past") {
+      url = "https://spacelaunchnow.me/api/3.3.0/launch/previous/?format=json";
+    }
+
+    var iteration = 1
+    var number = 3
+    if (args[0]) {
+      number = args[0]
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        var fieldValue = ""
+
+        for (var s in res.results) {
+          if (iteration > number) { break }
+
+          var launch = res.results[s];
+          var rocketName;
+          if (!launch.rocket.configuration.name) {
+            rocketName = "Unknown"
+          } else {
+            rocketName = launch.rocket.configuration.name
+          }
+
+          var agency;
+          if (!launch.rocket.configuration.launch_service_provider) {
+            agency = "Unknown"
+          } else {
+            agency = launch.rocket.configuration.launch_service_provider
+          }
+
+          var mission;
+          if (!launch.mission) {
+            mission = "Unknown"
+          } else {
+            mission = launch.mission.name
+          }
+
+          var site;
+          if (!launch.pad.name) {
+            site = "Unknown"
+          } else {
+            site = launch.pad.name
+          }
+
+          var date;
+          if (!launch.net) {
+            date = "Unknown"
+          } else {
+            date = new Date(launch.net)
+            date = date.toString().slice(16, 21) + "- " + date.toString().slice(4, 11)
+          }
+
+          var status = launch.status.name
+
+          var link;
+          if (!launch.slug) {
+            link = "unknown"
+          } else {
+            link = launch.slug
+          }
+
+          embed.addField(`**${rocketName}** - ${agency}`, `Mission name: ${mission}\nLaunch date: **${date}**\nLaunch site: ${site}`)
+          iteration++
+        }
+        embed.setDescription(`See [here](https://spacelaunchnow.me/launch/upcoming/) for more info`)
+        message.channel.send(embed)
+      })
+  }
+}
