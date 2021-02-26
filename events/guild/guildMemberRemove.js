d88b15154d59dab875acca02235d4fe3e2bf2a85
James Hackett <jamesthackett1@gmail.com>
2021-02-26 05:31:36 +0000

    :bug: Fix: fixes user leaves and kicks

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -18,32 +18,33 @@ module.exports = async (client, member) => {
     });
   }
 
+  const embed = new MessageEmbed()
   const fetchedLogs = await member.guild.fetchAuditLogs({
     limit: 1,
-    type: 'MEMBER_KICK',
   });
 
-  const embed = new MessageEmbed()
-
   const kickLog = fetchedLogs.entries.first();
-  if (!kickLog) {
+
+  if (!kickLog.action !== 'MEMBER_KICK') {
     embed.setTitle("**User Leave**")
       .setColor("RED")
-      .addField("User Left the guild", `${member.user.tag} left the guild`, true)
-    return logsChannel.send(embed)
-  }
-  const { executor, target } = kickLog;
-  if (target.id === member.id) {
-    embed.setTitle("**Kicked User**")
-      .setColor("RED")
-      .addField("User was kicked from the guild", `${member.user.tag} was kicked by ${executor.tag}`, true)
-      .setFooter(`Member ID: ${member.id}`)
-  } else {
-    embed.setTitle("**Kicked User**")
-      .setColor("RED")
-      .addField("User was kicked from the guild", `${member.user.tag} was kicked, but I don't know by who`, true)
-      .setFooter(`${member.id}`)
+      .setDescription(`**User Left the guild**\n${member} left the guild`)
   }
 
+  const { executor, target, action } = kickLog;
+
+  if (action === 'MEMBER_KICK') {
+    if (target.id === member.id) {
+      embed.setTitle("**Kicked User**")
+        .setColor("RED")
+        .setDescription(`**User was kicked from the guild**\n${member} was kicked by ${executor}`)
+        .setFooter(`Member ID: ${member.id}`)
+    } else {
+      embed.setTitle("**Kicked User**")
+        .setColor("RED")
+        .setDescription(`**User was kicked from the guild**\n${member} was kicked, but I don't know by who`)
+        .setFooter(`${member.id}`)
+    }
+  }
   logsChannel.send(embed)
 }
