df702ffb54248242013d3c5375b2f89818870db7
James Hackett <jamesthackett1@gmail.com>
2021-02-27 07:19:56 +0000

    :sparkles: Feat: adds a whisper command

---
+++ commands/dmcommands/whisper.js
@@ -0,0 +1,18 @@
+const GuildConfig = require('../../database/schemas/GuildConfig')
+
+module.exports = {
+  config: {
+    name: 'whisper',
+    aliases: ['w'],
+    usage: '<identifier> <message>',
+    category: 'dmcommands',
+    description: 'Sends your message anonymously to a channel.',
+    accessableby: 'Members',
+    permissions: '',
+    args: true,
+  },
+  run: async (client, message, args) => {
+    channel = client.channels.cache.get('815116475069235221')
+    channel.send(args.join(" "))
+  }
+}
