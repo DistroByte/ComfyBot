1d86fd193250e1938ba5c12568bba3753c4cf013
James Hackett <jamesthackett1@gmail.com>
2021-03-07 01:13:43 +0000

    Feat: adds logging etc

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const { sendLogs } = require("./utils/functions");
 const { Client, Collection } = require("discord.js");
 const { token, url, dbOptions, ownerid } = require("./botconfig.json");
 const client = new Client({ partials: ['MESSAGE', 'REACTION'] });
+
 const mongoose = require('mongoose');
-
 mongoose.connect(url, dbOptions);
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () { console.log("Connected to DB"); });
@@ -12,8 +12,15 @@ db.once('open', function () { console.log("Connected to DB"); });
 client.talkedRecently = new Set();
 client.emojiRoleMappings = {};
 client.ownerId = ownerid;
+client.messageCount = 0;
+client.commandCount = 0;
+
 ["authCodes", "cachedMessageReactions"].forEach(x => client[x] = new Map());
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
 
 client.login(token);
+
+setInterval(function () {
+  sendLogs(client)
+}, 30 * 1000)
--- utils/functions.js
+++ utils/functions.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 const { emailUser, emailPass } = require('../botconfig.json')
+const { logEvent } = require('./statistics')
 
 module.exports = {
   getMember: function (message, toFind = '') {
@@ -76,14 +77,43 @@ module.exports = {
     return levelXp = 5 * Math.floor(lvl / 1) ** 2 + 50 * Math.floor(lvl / 1) + 100
   },
   sendDuplicates: async function (message) {
-    message.channel.messages.fetch({ limit: 3 }).then(messages => {
+    message.channel.messages.fetch({ limit: 4 }).then(messages => {
       let values = messages.values()
+      let thirdLastMessage = values.next().value
       let secondLastMessage = values.next().value
       let lastMessage = values.next().value
-      if (!lastMessage.content || !secondLastMessage.content) return
-      if (lastMessage.content === secondLastMessage.content) {
+      if (!lastMessage.content || !secondLastMessage.content || !thirdLastMessage.content) return
+      if (lastMessage.content === secondLastMessage.content && lastMessage.content === thirdLastMessage.content) {
         message.channel.send(message.content)
       }
     })
   },
+  sendLogs: function (client) {
+    let textCount = 0
+    let voiceCount = 0
+    client.channels.cache.forEach(c => {
+      if (c.type === "text") {
+        textCount += 1
+      } else if (c.type === "voice") {
+        voiceCount += 1
+      }
+    })
+
+    let users = 0
+    client.guilds.cache.forEach(g => {
+      users += g.memberCount
+    })
+    logEvent(process.memoryUsage().rss, "bot_mem")
+    logEvent(client.guilds.cache.size, "guilds")
+    logEvent(users, "users")
+    logEvent(client.ws.ping, "ping")
+    logEvent(textCount, "channels_text")
+    logEvent(voiceCount, "channels_voice")
+    logEvent(client.channels.cache.size, "all_channels")
+    logEvent(client.uptime, "uptime")
+    logEvent(client.messageCount, "messages")
+    logEvent(client.commandCount, "commands")
+    client.commandCount = 0;
+    client.messageCount = 0;
+  }
 };
