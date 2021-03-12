ae0bf0ae90d3761cf6cc03e0d951c8f74bb0fa1a
James Hackett <jamesthackett1@gmail.com>
2021-03-12 15:02:34 +0000

    Feat: adds music player

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { sendLogs } = require("./utils/functions");
 const { Client, Collection } = require("discord.js");
-const { token, url, dbOptions, ownerid } = require("./botconfig.json");
+const { token, url, dbOptions, ownerid, emojis, filters } = require("./botconfig.json");
+const { Player } = require('discord-player');
 const client = new Client({ partials: ['MESSAGE', 'REACTION'] });
 
 const mongoose = require('mongoose');
@@ -9,11 +10,15 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () { console.log("Connected to DB"); });
 
+
 client.talkedRecently = new Set();
 client.emojiRoleMappings = {};
 client.ownerId = ownerid;
 client.messageCount = 0;
 client.commandCount = 0;
+client.player = new Player(client);
+client.emotes = emojis;
+client.filters = filters;
 
 ["authCodes", "cachedMessageReactions"].forEach(x => client[x] = new Map());
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
