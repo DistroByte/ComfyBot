fc4949895a0546f01235a33ec70442b85280ac3a
JHackett <jamesthackett1@gmail.com>
2021-04-08 03:36:14 +0100

    Bug: Fix: fixes small error

--- commands/General/getlines.js
+++ commands/General/getlines.js
@@ -14,15 +14,15 @@ class GetLines extends Command {
       memberPermissions: [],
       botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
       nsfw: false,
-      ownerOnly: false,
-      args: true,
+      ownerOnly: true,
+      args: false,
       cooldown: 3000
     });
   }
 
   async run(message, args, data) {
     let extension = args[0],
-      shell = bot.commands.get('exec'),
+      shell = this.client.commands.get('exec'),
       cmd = extension
         ? `git ls-files | grep "**.${extension}$" | xargs wc -l`
         : 'git ls-files | xargs wc -l';
