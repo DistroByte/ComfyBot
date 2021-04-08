20eff7833b9aefcfaa9441478080134753e23268
JHackett <jamesthackett1@gmail.com>
2021-04-08 03:54:30 +0100

    Feat: adds meme command

---
+++ commands/Images/meme.js
@@ -0,0 +1,50 @@
+const Command = require("../../base/Command"),
+  { MessageEmbed } = require('discord.js'),
+  api = require('imageapi.js');
+
+class Meme extends Command {
+  constructor(client) {
+    super(client, {
+      name: "meme",
+      description: "Gets a meme from a subreddit!",
+      dirname: __dirname,
+      enabled: true,
+      guildOnly: false,
+      aliases: [],
+      memberPermissions: [],
+      botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+      nsfw: false,
+      ownerOnly: false,
+      args: false,
+      cooldown: 3000
+    });
+  }
+
+  async run(message, args, data) {
+    let subreddit = [
+      "2meirl4meirl",
+      "AdviceAnimals",
+      "AnimeFunny",
+      "MemeEconomy",
+      "MemesOfAnime",
+      "animememes",
+      "animemes",
+      "dankmeme",
+      "dankmemes",
+      "me_irl",
+      "meirl",
+      "meme",
+      "techsupportanimals",
+      "wholesomememes"
+    ].random(),
+      image = await api(subreddit),
+      embed = new MessageEmbed()
+        .setTitle(`A Meme From ${subreddit}`)
+        .setColor("RANDOM")
+        .setImage(image);
+
+    message.channel.send(embed);
+  }
+}
+
+module.exports = Meme;
