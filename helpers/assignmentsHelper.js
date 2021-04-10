e173bffd1bd2648117cbfa6cf8cc3486081a94f5
JHackett <jamesthackett1@gmail.com>
2021-04-10 06:52:34 +0100

    Feat: Fixes embed display

--- helpers/assignmentsHelper.js
+++ helpers/assignmentsHelper.js
@@ -76,7 +76,7 @@ async function UpdateAssignmentsEmbed(client) {
   for (i in assignmentsData) {
     let NewAssignment = assignmentsData[i]
     let moduleName = NewAssignment.moduleName || await FetchModuleNameFromCode(NewAssignment.moduleCode)
-    embedContent += `${NewAssignment.assignmentID}. **${NewAssignment.moduleCode} - ${moduleName.slice(9)}**\n${NewAssignment.description}\nDue *${NewAssignment.dueDate.toString().slice(0, "Fri Apr 09 2021 23:59:00".length)}* - in ${NewAssignment.Countdown}\n\n`
+    embedContent += `${NewAssignment.assignmentID}. **${NewAssignment.moduleCode} - ${moduleName.slice(9)}**\n${NewAssignment.description}\nDue *${NewAssignment.dueDate.toString().slice(0, "Fri Apr 09 2021 23:59".length)}* - in ${NewAssignment.Countdown}.\n\n`
   }
 
   let AssignmentsChannel = await client.channels.cache.get("829045215679610891")
