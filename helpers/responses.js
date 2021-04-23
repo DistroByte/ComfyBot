a8b048fa8bee0f33290fe9679a0a70b6f6f69675
JHackett <jamesthackett1@gmail.com>
2021-04-23 01:52:28 +0100

    Create Standardised Responses WIP

---
+++ helpers/responses.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "ERROR.TIME": "Times up! Please run the command again!",
+  "ERROR.CHANNEL": "Please specify a channel!",
+  "ERROR.USER": "Please mention a user!",
+  "ERROR.ERROR": "Something went wrong, please try again!",
+  "ERROR.TIMEARGS": "Please enter a valid time! Available units: `s`, `m`, `h` or`d`",
+  "ERROR.EMPTY": "Please enter a message!",
+  "ERROR.NOID": "I can't find that id! Please try again!",
+  "ERROR.NOREASON": "No reason provided!"
+}
