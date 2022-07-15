311378aac0669a1ee50255aeb01e4122a66aca1c
DistroByte <jamesthackett1@gmail.com>
2022-07-15 03:17:21 +0100

    updates cooldown

--- events/message/message.js
+++ events/message/message.js
@@ -74,15 +74,15 @@ module.exports = class {
         if (message.content.toLowerCase().trim().split(/ +/g).includes("u")) {
           let insult = generateInsult();
           if (insultCooldown > Date.now()) return;
-          insultCooldown = Date.now() + 120000;
-          message.reply(`"You" ${insult}, "you"`)
+          insultCooldown = Date.now() + 900000;
+          message.reply(`"You" ${insult}, "you"`);
         }
 
         if (message.content.toLowerCase().trim().split(/ +/g).includes("y")) {
           let insult = generateInsult();
           if (insultCooldown > Date.now()) return;
-          insultCooldown = Date.now() + 120000;
-          message.reply(`"Why" ${insult}, "why"`)
+          insultCooldown = Date.now() + 900000;
+          message.reply(`"Why" ${insult}, "why"`);
         }
 
         if (message.content.toLowerCase().includes("finland has an army?")) {
@@ -284,8 +284,8 @@ async function updateXp(msg, data) {
 }
 
 function generateInsult() {
-  const one = require('../../helpers/partone.json');
-  const two = require('../../helpers/parttwo.json');
+  const one = require("../../helpers/partone.json");
+  const two = require("../../helpers/parttwo.json");
 
-  return `${one[Math.floor(Math.random() * one.length)]}${two[Math.floor(Math.random() * two.length)]}`
+  return `${one[Math.floor(Math.random() * one.length)]}${two[Math.floor(Math.random() * two.length)]}`;
 }
